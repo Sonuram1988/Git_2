554125da1802c46589589dff484c2ac1cbcba01f
shiva <shivkartik1@gmail.com>
2022-11-02 22:44:44 +0530

    add a

--- hello.java
+++ hello.java
@@ -2,5 +2,6 @@ class Main{
     public static void name(String args[]) {
         System.out.println("Hello World!!");
         System.out.println("Welcome in dev branch");
+        int a=10;
     }
 }
